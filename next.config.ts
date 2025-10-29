f26f0396601ec54b1ab30ad06d34f540928f29e6
Yagnik-Gohil <gohilyagnik3@gmail.com>
2025-10-29 19:37:30 +0530

    feat: initial setup

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.cloudfront.net",
+      },
+    ],
+  },
+  trailingSlash: true,
+  output: 'export'
 };
 
 export default nextConfig;
